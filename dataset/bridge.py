35c0fbacd26a5be8db299a404320015bad6d704d
Juanjo <kendixd@gmail.com>
2017-10-26 13:13:18 +0200

    created dictionary with dataset info

--- dataset/bridge.py
+++ dataset/bridge.py
@@ -10,6 +10,8 @@ def load_data(percent_of_damage=0.2):
     patch_dir = '100/'
     deck_dirs = ['deck_a/', 'deck_c/', 'deck_d/', 'deck_e/']
     image_list, label_list = [], []
+    data_dict = {}
+    img_id = 0
     for deck_dir in deck_dirs:
         mask_fpath = os.path.join(root_dir, dataset, patch_dir, deck_dir)
         image_dirs = os.listdir(mask_fpath)
@@ -28,23 +30,36 @@ def load_data(percent_of_damage=0.2):
                 mask_path = os.path.join(mask_fpath, image_dir, image_name)
                 img = np.asarray(Image.open(img_path))
                 mask = np.asarray(Image.open(mask_path))
-                image_list.append(img)
                 damage_pixels = (mask / 255).sum()
-                if damage_pixels > 100 * 100 * percent_of_damage:
-                    label_list.append(1)
+                if damage_pixels > percent_of_damage * mask.size:
+                    label = 1
                 else:
-                    label_list.append(0)
-    x = np.array(image_list)
-    y = np.array(label_list)[:, np.newaxis]
-    x_train, x_test, y_train, y_test = train_test_split(
-        x,
-        y,
-        test_size=0.33,
-        random_state=42
-    )
+                    label = 0
+                data_dict[img_id] = {'path' : img_path, 'mask_path' : mask_path,
+                                'img' : img, 'mask' : mask, 'label' : label }
+                img_id += 1
 
-    return (x_train, y_train), (x_test, y_test)
+    train_set_idcs, test_set_idcs = train_test_split(list(data_dict.keys()), test_size=0.33)
+
+    x_train = []
+    y_train = []
+    for idx in train_set_idcs:
+        x_train.append(data_dict[idx]['img'])
+        y_train.append(data_dict[idx]['label'])
+
+    x_test = []
+    y_test = []
+    for idx in test_set_idcs:
+        x_test.append(data_dict[idx]['img'])
+        y_test.append(data_dict[idx]['label'])
+
+    x_train = np.array(x_train)
+    x_test = np.array(x_test)
+    y_train = np.array(y_train)[:,np.newaxis]
+    y_test = np.array(y_test)[:,np.newaxis]
+    return data_dict, train_set_idcs, test_set_idcs, x_train, y_train, \
+            x_test, y_test
 
 
 if __name__ == '__main__':
-    (x_train, y_train), (x_test, y_test) = load_data()
+    data_dict, train_set_idcs, test_set_idcs, x_train, y_train, x_test, y_test = load_data()
