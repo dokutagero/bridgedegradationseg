b923bb4c8d81b91ee063e2bd4f74091426e9c21d
dokutagero <kendixd@gmail.com>
2017-11-08 15:40:59 +0900

    cropping images for testing model with fixed-sized inputs

--- train_FCN.py
+++ train_FCN.py
@@ -12,35 +12,52 @@ def load_full_images(datapath):
 	imgs = []
 	masks = []
 	for root, dirs, files in os.walk(datapath):
-		if root.split('/')[-2] == bridge_masks_path:
-			# print(root)
-			for f in files:
-				mask = Image.open(os.path.join(root, f))
-				imgpath = os.path.join(root.split('/')[0], bridge_images_path, root.split('/')[-1], f)
-				img = Image.open(imgpath)
-				mask = np.asarray(mask)
-				mask.flags.writeable = True
-				masks.append(mask)
-				img = np.asarray(img)
-				img.flags.writeable = True
-				imgs.append(img)
-	
+            if root.split('/')[-1] in ['deck_a', 'deck_c', 'deck_d', 'deck_e']:
+                print(root)
+                for f in files:
+                        mask = Image.open(os.path.join(root, f))
+                        mask = crop_center(mask)
+                        imgpath = os.path.join(root.split('/')[0], bridge_images_path, root.split('/')[-1], f.split('.')[-2]+'.jpg')
+                        img = Image.open(imgpath)
+                        img = crop_center(img)
+                        mask = np.asarray(mask)
+                        mask.flags.writeable = True
+                        masks.append(mask)
+                        img = np.asarray(img)
+                        img.flags.writeable = True
+                        imgs.append(img)
 	return imgs, masks
-			
+
+def crop_center(img):
+    half_width = img.size[0] // 2
+    half_height = img.size[1] // 2
+
+    w = 1024//2
+    h = 768//2
+
+    cropped_img = img.crop(
+            (
+                half_width - w,
+                half_height - h,
+                half_width + w,
+                half_height + h
+            )
+        )
+    return cropped_img
+
+
 if __name__ == "__main__":
-    model = FCN_Vgg16_32s(input_shape=(None, None, 3), classes=2)
+    # model = FCN_Vgg16_32s(input_shape=(None, None, 3), classes=2)
 
     # import data
     datapath = sys.argv[1]
     data_augmentation = False
-
-
     epochs = sys.argv[2]
-    
+
     imgs, masks = load_full_images(datapath)
     np_imgs = [im/255 for im in imgs]
     #np_masks = [m[np.where(m > 0)]=1 for m in masks]
     # Correcting gray values from jpg compression. Save them again?
     for mask in masks:
-        mask[mask > 0] = 1 
-   
+        mask[mask > 0] = 1
+
