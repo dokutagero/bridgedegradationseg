2442eafd9409148bdc1792bc84e3eeb8e9c2865b
dokutagero <kendixd@gmail.com>
2017-11-09 18:10:16 +0900

    data generator for avoiding to load all images on memory. Very simple generator from txt file

--- utils/datagenerator.py
+++ utils/datagenerator.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+from utils.preprocess import crop_center
 from PIL import Image
 import os, sys
 
@@ -22,8 +23,24 @@ def dataset_to_txt(datapath):
         txtfile.write('\n'.join(paths))
 
 
+def generator_from_txt(txtdatapath):
+    while 1:
+        f = open(txtdatapath)
+        for line in f:
+            imgpath, maskpath = line.rstrip().split(',')
+            mask = Image.open(maskpath)
+            mask = crop_center(mask)
+            img = Image.open(imgpath)
+            img = crop_center(img)
+
+            yield np.expand_dims(np.array(img), axis=0), np.expand_dims(np.array(mask)[np.newaxis], axis=-1)
+
 
 if __name__ == "__main__":
     
     datapath = sys.argv[1]
-    dataset_to_txt(datapath)
+    # dataset_to_txt(datapath)
+    a = generator_from_txt(datapath)
+    for img, mask in a:
+        print(img.shape,mask.shape)
+        break
