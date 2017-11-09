5813c6e33fdd59ffa94ec7ea15c6e90d995548b6
dokutagero <kendixd@gmail.com>
2017-11-09 18:09:27 +0900

    added full images loading function and center cropping function (will remove from main train file)

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -45,5 +45,46 @@ def main():
     patch_size = int(sys.argv[2])
     save_patches(datapath, patch_size)
 
+
+def load_full_images(datapath):
+	bridge_masks_path = 'bridge_masks'
+	bridge_images_path = 'images'
+	imgs = []
+	masks = []
+	for root, dirs, files in os.walk(datapath):
+            if root.split('/')[-1] in ['deck_a', 'deck_c', 'deck_d', 'deck_e']:
+                print(root)
+                for f in files:
+                        mask = Image.open(os.path.join(root, f))
+                        mask = crop_center(mask)
+                        imgpath = os.path.join(root.split('/')[0], bridge_images_path, root.split('/')[-1], f.split('.')[-2]+'.jpg')
+                        img = Image.open(imgpath)
+                        img = crop_center(img)
+                        mask = np.asarray(mask)
+                        mask.flags.writeable = True
+                        masks.append(mask)
+                        img = np.asarray(img)
+                        img.flags.writeable = True
+                        imgs.append(img)
+	return imgs, masks
+
+
+def crop_center(img):
+    half_width = img.size[0] // 2
+    half_height = img.size[1] // 2
+
+    w = 1024//2
+    h = 768//2
+
+    cropped_img = img.crop(
+            (
+                half_width - w,
+                half_height - h,
+                half_width + w,
+                half_height + h
+            )
+        )
+    return cropped_img
+
 if __name__=="__main__":
     main()
